e1f511bbd63adf507a97574e7669a40033a96dd2
mpg <mpg@flaxen.com>
2017-07-31 12:05:54 -0400

    WIP

--- app/main.go
+++ app/main.go
@@ -14,114 +14,138 @@ func main() {
 	app.Name = "beachfront"
 	app.Usage = "access the Beachfront services"
 
-	var feedName, sceneId string
-
 	app.Commands = []cli.Command{
 		{
-			Name:    "feedinfo",
-			Aliases: []string{"fi"},
-			Usage:   "get info about a feed or scene",
-
-			Subcommands: []cli.Command{
-				{
-					Name:    "feedinfo",
-					Aliases: []string{"fi"},
-					Usage:   "get info about a feed or scene",
-
-					Subcommands: []cli.Command{
-						{
-							Name:    "feedinfo",
-							Aliases: []string{"fi"},
-							Usage:   "get info about a feed or scene",
-
-							Flags: []cli.Flag{
-								cli.StringFlag{
-									Name:        "feed,f",
-									Usage:       "name of the feed (required)",
-									Destination: &feedName,
-								},
-								cli.StringFlag{
-									Name:        "scene,s",
-									Usage:       "id of the scene in the feed (optional)",
-									Destination: &sceneId,
-								},
-							},
-							Action: func(c *cli.Context) error {
-								switch {
-								case c.IsSet("feed") && c.IsSet("scene"):
-									return runFeedInfoScene(feedName, sceneId)
-								case c.IsSet("feed"):
-									return runFeedInfoFeed(feedName)
-								default:
-									return cli.NewExitError("feed info: either '--feed' or '-feed --scene' is required", 2)
-								}
-							},
-						},
-					},
-				},
-			},
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:        "feed,f",
-					Usage:       "name of the feed (required)",
-					Destination: &feedName,
-				},
-				cli.StringFlag{
-					Name:        "scene,s",
-					Usage:       "id of the scene in the feed (optional)",
-					Destination: &sceneId,
-				},
-			},
-		},
-		{
-			Name:    "feeddownload",
-			Aliases: []string{"fd"},
-			Usage:   "download file(s) for a scene",
+			Name:    "catalog",
+			Aliases: []string{"cat"},
+			Usage:   "access catalog (imagery feed) services",
 
 			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:        "feed,f",
-					Usage:       "name of the feed (required)",
-					Destination: &feedName,
+				cli.BoolFlag{
+					Name:  "info,i",
+					Usage: "get information about a scene, a catalog, or all catalogs",
 				},
 				cli.StringFlag{
-					Name:        "scene,s",
-					Usage:       "id of the scene in the feed (optional)",
-					Destination: &sceneId,
+					Name:  "download,d",
+					Usage: "downoad a scene from a catalog",
 				},
 			},
 			Action: func(c *cli.Context) error {
+				info := c.IsSet("info")
+				download := c.IsSet("download")
+
 				switch {
-				case c.IsSet("feed") && c.IsSet("scene"):
-					return runFeedDownload(feedName, sceneId)
+				case info && !download:
+					arg0, err := getZeroOrOneArg("catalog info", c)
+					if err != nil {
+						return err
+					}
+					return runCatalogInfo(arg0)
+				case !info && download:
+					arg, err := getOneArg("catalog download", c)
+					if err != nil {
+						return err
+					}
+					return runCatalogDownload(arg)
 				default:
-					return cli.NewExitError("feed download: --feed and --scene are both required", 2)
+					return cli.NewExitError("catalog: exactly one of --info and --download is required", 2)
 				}
 			},
 		},
 		{
-			Name:    "jobinfo",
-			Aliases: []string{"ji"},
-			Usage:   "get information about a job",
+			Name: "job",
+			//Aliases: []string{""},
+			Usage: "access job services",
 
 			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:        "feed,f",
-					Usage:       "name of the feed (required)",
-					Destination: &feedName,
+				cli.BoolFlag{
+					Name:  "info,i",
+					Usage: "get information about a job or all jobs",
 				},
-				cli.StringFlag{
-					Name:        "scene,s",
-					Usage:       "id of the scene in the feed (optional)",
-					Destination: &sceneId,
+				cli.BoolFlag{
+					Name:  "submit,s",
+					Usage: "submit a new job for execution",
+				},
+				cli.BoolFlag{
+					Name:  "delete,d",
+					Usage: "delete a job",
 				},
 			},
 			Action: func(c *cli.Context) error {
+				info := c.IsSet("info")
+				submit := c.IsSet("submit")
+				delete := c.IsSet("delete")
+
 				switch {
-				case c.IsSet("feed") && c.IsSet("scene"):
-					return runFeedDownload(feedName, sceneId)
+				case info && !submit && !delete:
+					arg, err := getZeroOrOneArg("job info", c)
+					if err != nil {
+						return err
+					}
+					return runJobInfo(arg)
+				case !info && submit && !delete:
+					return runJobSubmit()
+				case !info && !submit && delete:
+					arg, err := getOneArg("job deletion", c)
+					if err != nil {
+						return err
+					}
+					return runJobDelete(arg)
 				default:
-					return cli.NewExitError("feed download: --feed and --scene are both required", 2)
+					return cli.NewExitError("job: exactly one of --info and --submit is required", 2)
+				}
+			},
+		},
+		{
+			Name:    "coastline",
+			Aliases: []string{"coast"},
+			Usage:   "access coastline data",
+
+			Flags: []cli.Flag{
+				cli.BoolFlag{
+					Name:  "download,d",
+					Usage: "download the geojson coastline file",
+				},
+			},
+			Action: func(c *cli.Context) error {
+				download := c.IsSet("download")
+
+				switch {
+				case download:
+					arg, err := getOneArg("coastline download", c)
+					if err != nil {
+						return err
+					}
+					return runCoastlineDownload(arg)
+				default:
+					return cli.NewExitError("coastline: --download is required", 2)
+				}
+			},
+		},
+		{
+			Name:    "algorithm",
+			Aliases: []string{"alg"},
+			Usage:   "access the algorithm services",
+
+			Flags: []cli.Flag{
+				cli.BoolFlag{
+					Name:  "info,i",
+					Usage: "get information about an algorithm or all algorithms",
+				},
+			},
+			Action: func(c *cli.Context) error {
+				info := c.IsSet("info")
+
+				arg, err := getZeroOrOneArg("algorithm info", c)
+				if err != nil {
+					return err
+				}
+
+				switch {
+				case info:
+					return runAlgorithmInfo(arg)
+				default:
+					return cli.NewExitError("algorithm: --info is required", 2)
 				}
 			},
 		},
@@ -137,14 +161,52 @@ func main() {
 
 //---------------------------------------------------------------------
 
-func runFeedDownload(feedName string, sceneId string) error {
+func getZeroOrOneArg(area string, c *cli.Context) (string, error) {
+	switch c.NArg() {
+	case 0:
+		return "", nil
+	case 1:
+		return c.Args().Get(0), nil
+	default:
+		return "", cli.NewExitError(area+": exactly zero or one argument is required", 2)
+	}
+}
+
+func getOneArg(area string, c *cli.Context) (string, error) {
+	switch c.NArg() {
+	case 1:
+		return c.Args().Get(0), nil
+	default:
+		return "", cli.NewExitError(area+": exactly one argument is required", 2)
+	}
+}
+
+//---------------------------------------------------------------------
+
+func runCatalogInfo(id string) error {
 	return nil
 }
 
-func runFeedInfoScene(feedName string, sceneId string) error {
+func runCatalogDownload(id string) error {
 	return nil
 }
 
-func runFeedInfoFeed(feedName string) error {
+func runJobInfo(id string) error {
+	return nil
+}
+
+func runJobSubmit() error {
+	return cli.NewExitError("job: --submit not yet supported", 2)
+}
+
+func runJobDelete(id string) error {
+	return cli.NewExitError("job: --delete not yet supported", 2)
+}
+
+func runCoastlineDownload(id string) error {
+	return nil
+}
+
+func runAlgorithmInfo(id string) error {
 	return nil
 }
